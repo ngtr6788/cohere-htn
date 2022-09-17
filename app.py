b16fc9d29ef834c11c8294d2c43cf1f15b7e90b4
Nguyen Tran <nguyentran1072e@gmail.com>
2022-09-17 18:14:14 -0400

    Fixed CORS issue with backend

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from typing import List
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # In real life, production code, not recommended, don't do this
 
-
-@app.post("/search")
+@app.route("/search", methods=["POST"])
 def search():
     if request.is_json:
         json_request = request.get_json()
         text_path = json_request["textPath"]
-        keywords: List[str] = json_request["keywords"]
+        keywords = json_request["keywords"]
 
         # TODO: Integrate this HTTP endpoint with the semantic search logic
         return {"textPath": text_path, "keywords": keywords}
+    else:
+        return {"error": "Request must be JSON"}, 415
